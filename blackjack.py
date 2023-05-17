7d100cd44a9a70a0f350a056be059e73eccc3d41
Exo30 <mstucke2@ncsu.edu>
2023-05-17 11:43:51 -0400

    added multiple player functionality

--- blackjack.py
+++ blackjack.py
@@ -10,11 +10,6 @@ class Card:
         self.rank = rank
     def __repr__(self):
         return f'{self.rank} of {self.suit}  '
-    #def __str__(self):
-        #return f'{self.rank} of {self.suit}'
-    
-#deck_of_cards = [Card(suit, rank) for suit in SUITS for rank in RANKS]
-#print(deck_of_cards)
 
 class Deck:
     def __init__(self, suits, ranks):
@@ -43,7 +38,7 @@ class Player:
         self.hand = []
         self.name = name
         self.total = 0
-        self.stayed = False
+        self.stay = False
         self.bust = False
     
     def __str__(self):
@@ -55,21 +50,20 @@ class Player:
             card = str(card)
             num = [int(s) for s in re.findall(r'\b\d+\b', card)]
             if num != []:
-                self.total += int(card[0])
-            if card[0].find('A') != -1:
+                self.total += int(num[0])
+                if self == 'Matt':
+                    print(self.total)
+            elif card[0].find('A') != -1:
                 if self.total + 11 > 21:
                     self.total += 1
                 else:
                     self.total += 11
-            if (card[0].find('J') != -1) or (card[0].find('K') != -1) or (card[0].find('Q') != -1):
-                self.total += 11
+            else:
+                self.total += 10
 
     def hit(self, card):
         self.hand.append(card)
         self.calc_total()
-
-    def stay(self):
-        pass
 
     def money(self, money):
         self.money = money
@@ -82,7 +76,7 @@ class Dealer(Player):
         self.name = 'Dealer'
         self.hand = []
         self.total = 0
-        self.stayed = False
+        self.stay = False
         self.bust = False
     
     def __str__(self):
@@ -92,13 +86,27 @@ class Dealer(Player):
 
 class Game: 
     def __init__(self, suits, ranks):
-        self.player = Player(self.get_player_name())
+        self.play_num = self.get_player_count()
+        self.play_count = 0
+        self.players = []
+        while self.play_count != self.play_num:
+            self.players.append(Player(self.get_player_name()))
+            self.play_count += 1
+            #self.eval(f'{"_".join(["player", str(self.play_count)])}') = Player(self.get_player_name())
+            print("got this far")
         self.dealer = Dealer()
         self.deck = Deck(suits, ranks)
-    
+        self.done = 0
+        self.dealer_done = False
+        self.bust_count = 0
+
     def get_player_name(self):
-        name = input('What is your name?')
+        name = input('What is your name?   ')
         return name
+
+    def get_player_count(self):
+        count = input("How many players are playing?  ")
+        return int(count)
     
     def draw(self, person):
         self.person = person
@@ -114,40 +122,104 @@ class Game:
         person.calc_total()
 
     def turn(self, person):
-        if person.bust == False and person.stayed == False:
-            if person == self.dealer:
-                if person.total < 16:
-                    self.draw(person)
-                else:
-                    person.stayed = True
-            if person == self.player:
-                print(person.hand)
-                print(person.total)
-                turn = input('Stay, Hit or Bet?   ')
-                if turn == 'stay' or turn == 'Stay' or turn == 'STAY':
-                    person.stayed = True
-                if turn == 'hit' or turn == 'HIT' or turn =='Hit':
-                    self.draw(person)
-                    print(person.total)
-                    if person.total > 21:
-                        person.bust = True
+        print("#############################################")
+        print("#############################################")
+        if person == self.dealer:
+            if person.total < 16:
+                self.draw(person)
+            if person.total > 21:
+                person.bust = True
+                self.dealer_done = True
+            elif person.total >= 16: 
+                person.stay = True
+                self.dealer_done = True                      
+        else:
+            print(person.name + "'s hand:  " + str(person.hand))
+            turn = input('Stay, Hit or Bet?   ')
+            if turn == 'stay' or turn == 'Stay' or turn == 'STAY':
+                person.stay = True
+                self.done += 1 
+            if turn == 'hit' or turn == 'HIT' or turn =='Hit':
+                self.draw(person)
+                print('Player total is : ' + str(person.total))
+                if person.total > 21:
+                    person.bust = True
+                    self.done += 1 
+                    self.bust_count += 1
 
-    def play(self, players):
+    def determine_winner(self, players):
+        winners = []
+        winner_total = 0
+        if (self.done == len(players)):
+            if (self.dealer.bust == False):
+                winners.append(self.dealer)
+                winner_total = self.dealer.total
+            if (len(players) == 1):
+                if players[0].bust == False:
+                    if players[0].total > winner_total:
+                        winner_total = players[0].total
+                        winners = [(players[0])]
+                    elif players[0].total == winner_total:
+                        winners.append(players[0])
+            else:
+                for player in players:
+                    if player.bust == False:
+                        print(player.total, winner_total, player.total > winner_total)
+                        if player.total > winner_total:
+                            winner_total = player.total
+                            winners = [player]
+                        elif player.total == winner_total:
+                            if str(player) in winners == False:
+                                winners.append(player)
+                                print('Should be tie  ' + str(winners))
+        if (winners != []):
+            if (winners[0] != self.dealer and self.dealer_done == False):
+                self.turn(self.dealer)
+                return self.determine_winner(players)
+            elif(len(winners) > 1):
+                print("We've got a " + str(len(winners)) + "-way tie!!! ")
+                for player in winners:
+                    print(str(player))
+                print("All of these are winners! The pot will be split")
+                return ''
+            elif(len(winners) == 1):
+                print(str(winners[0]) + ' is the winner!!! With a total of ' + str(winners[0].total) +  ' and a hand of ' + str(winners[0].hand))
+                return ''
+            else:
+                print("We've got an error boys")
+                return ''
+        if (winners == [] and self.dealer.bust == True and self.bust_count == len(players)):
+            print('Everyone busts and the House wins!!!!')
+            return ''
+        winners = []
+        return winners
+
+    def round(self, players):
+        print("#############################################")
+        print("#############################################")
+        for person in players:
+            if person.bust == False:
+                pass
+                # self.determine_winner(players)
+            if person.bust == False and person.stay == False:
+                self.turn(person)
+
+
+    def play(self, players, dealer):
         for player in players:
-            new_game.start_draw(player)
+            self.start_draw(player)
+        self.start_draw(dealer)
+        winner = []
+
+        while (winner == []):
+            self.round(players)
+            winner = self.determine_winner(players)
 
 
-#deck_of_cards = Deck(SUITS, RANKS)
-#deck_of_cards.shuffle()
 
-#print(deck_of_cards.draw())
+
+
 new_game = Game(SUITS, RANKS)
 new_game.deck.shuffle()
-count = 0
-#print(new_game.deck.cards)
-new_game.play([new_game.player, new_game.dealer])
-new_game.turn(new_game.player)
-new_game.turn(new_game.dealer)
-#print(new_game.deck.cards)
-#print(Card('heart', 'two'))
-#print(len(new_game.deck.cards))
+new_game.play(new_game.players, new_game.dealer)
+
