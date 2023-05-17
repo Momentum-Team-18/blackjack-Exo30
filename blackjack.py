60a9d4b8200031f48199b2176f063e3950c16502
Exo30 <mstucke2@ncsu.edu>
2023-05-17 16:04:01 -0400

    addded betting functionality, the game will now not end until all players are out, a new deck will be created and shuffled when cards run out of original deck -----NOT TESTED YET, BUGS ARE PROBABLY EVERYWHERE

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,6 @@
 import random
 import re
+import math
 # Write your blackjack game here.
 SUITS = ['❤️', '♣️', '♦️', '♠️']
 RANKS = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
@@ -31,6 +32,10 @@ class Deck:
         random.shuffle(self.cards)
     
     def draw(self):
+        if len(self.cards) == 0:
+            for suit in self.suits:
+                for rank in self.ranks:
+                    self.cards.append(Card(suit, rank))
         return self.cards.pop(0)
 
 class Player:
@@ -40,27 +45,32 @@ class Player:
         self.total = 0
         self.stay = False
         self.bust = False
-        self.money = 10
+        self.wins = 0
+        self.out = False
     
     def __str__(self):
         return self.name
 
     def calc_total(self):
         self.total = 0
+        self.ace = 0
         for card in self.hand:
             card = str(card)
             num = [int(s) for s in re.findall(r'\b\d+\b', card)]
             if num != []:
                 self.total += int(num[0])
-                if self == 'Matt':
-                    print(self.total)
             elif card[0].find('A') != -1:
-                if self.total + 11 > 21:
-                    self.total += 1
-                else:
-                    self.total += 11
+                self.ace += 1
             else:
                 self.total += 10
+        while self.ace != 0:
+            if self.total + 11 > 21:
+                self.total += 1
+            else:
+                self.total += 11
+            self.ace -= 1
+        if self.total > 21:
+            self.bust = True
 
     def hit(self, card):
         self.hand.append(card)
@@ -77,6 +87,7 @@ class Dealer(Player):
         self.total = 0
         self.stay = False
         self.bust = False
+        self.wins = 0
     
     def __str__(self):
         return 'Dealer'
@@ -91,12 +102,18 @@ class Game:
         while self.play_count != self.play_num:
             self.players.append(Player(self.get_player_name()))
             self.play_count += 1
-            #self.eval(f'{"_".join(["player", str(self.play_count)])}') = Player(self.get_player_name())
         self.dealer = Dealer()
         self.deck = Deck(suits, ranks)
         self.done = 0
         self.dealer_done = False
         self.bust_count = 0
+        self.pot = 0
+        self.entry_bet = 3
+        self.players_out = 0
+        self.starting_money = 10
+        self.game_over = False
+        for player in self.players:
+            player.money = self.starting_money
 
     def get_player_name(self):
         name = input('What is your name?   ')
@@ -114,25 +131,11 @@ class Game:
         person.calc_total()
 
     def start_draw(self, person):
-        person.hit(self.deck.draw())
-        person.hit(self.deck.draw())
-        print(str(person) + "'s hand:  " + str(person.hand))
-        person.calc_total()
-
-    def call_round(self, better, players, bet):
-        for person in players:
-            if person != better:
-                next_bet = input(str(person) + "'s Bet!!  Would you like to (c)all, (r)aise or (f)old?  ")
-                if next_bet == "c":
-                    pass
-                if next_bet =="r":
-                    new_bet = input("How much would you like to bet?  ")
-                    if new_bet > bet:
-                        self.call_round(person, players, new_bet)
-                    else:
-                        print("Please input a valid bet")
-                if next_bet == 'f':
-                    person.bust = True
+        if person.bust == False:
+            person.hit(self.deck.draw())
+            person.hit(self.deck.draw())
+            print(str(person) + "'s hand:  " + str(person.hand))
+            person.calc_total()
 
     def turn(self, person):
         print("#############################################")
@@ -148,7 +151,7 @@ class Game:
                 self.dealer_done = True                      
         else:
             print(person.name + "'s hand:  " + str(person.hand))
-            turn = input('(s)tay, (h)it or (b)et?   ')
+            turn = input('(s)tay, or (h)it?   ')
             if turn == 's':
                 person.stay = True
                 self.done += 1 
@@ -159,14 +162,6 @@ class Game:
                     person.bust = True
                     self.done += 1 
                     self.bust_count += 1
-            if turn == "b":
-                bet = input("How much are you betting? (Enter c to cancel)   ")
-                if bet == 'c':
-                    return turn(self, person)
-                else:
-                    bet = int(bet)
-                    if bet > 0:
-                        return bet
 
     def determine_winner(self, players):
         winners = []
@@ -184,8 +179,7 @@ class Game:
                         winners.append(players[0])
             else:
                 for player in players:
-                    if player.bust == False:
-                        print(player.total, winner_total, player.total > winner_total)
+                    if player.bust == False and player.total <= 21:
                         if player.total > winner_total:
                             winner_total = player.total
                             winners = [player]
@@ -198,13 +192,18 @@ class Game:
                 self.turn(self.dealer)
                 return self.determine_winner(players)
             elif(len(winners) > 1):
+                split = math.floor(self.pot/int(len(winners)))
                 print("We've got a " + str(len(winners)) + "-way tie!!! ")
                 for player in winners:
+                    player.money += split
                     print(str(player))
                 print("All of these are winners! The pot will be split")
                 return ''
             elif(len(winners) == 1):
                 print(str(winners[0]) + ' is the winner!!! With a total of ' + str(winners[0].total) +  ' and a hand of ' + str(winners[0].hand))
+                if winners[0] != self.dealer:
+                    winners[0].money += self.pot
+                winners[0].wins += 1
                 return ''
             else:
                 print("We've got an error boys")
@@ -213,30 +212,86 @@ class Game:
             print('Everyone busts and the House wins!!!!')
             return ''
         winners = []
+        print("whoops")
         return winners
 
     def round(self, players):
         print("#############################################")
+        print("-----------------NEW ROUND-------------------")
         print("#############################################")
         for person in players:
             if person.bust == False:
                 pass
-                # self.determine_winner(players)
             if person.bust == False and person.stay == False:
                 turn = self.turn(person)
                 if turn and turn > 0:
                     self.call_round(person, players, turn)
+    
+    def start_bet(self, player):
+        print("You have " + str(player.money) + " left to bet")
+        if player.money >= self.entry_bet:
+            player.money -= self.entry_bet
+            self.pot += self.entry_bet
+        else:
+            player.bust == True
+            self.done += 1
+            self.players_out += 1
+            player.out = True
+            print(str(player) + ' is out of the game!!!')
+        if player.money >= self.starting_money * 5:
+            print("You have won five times more than what you started with, CONGRATS!!! However, The House would like a word with you in the back......")
+            player.bust == True
+            self.done += 1
+            self.players_out += 1
+            print(str(player) + ' is out of the game!!!')
+            player.out = True
 
+
+    def special_options(self, player):
+        double = input("If you like your hand you can double down on your bet.... (y)es or (n)o? ")
+        if double == 'y':
+            player.stay = True
+            self.done += 1
+            player.money - self.entry_bet
+            self.pot += self.entry_bet + self.entry_bet
+            self.draw(player)
+
+    def cash_out(self, player):
+        question = input(str(player) + ' would you like to cash out? (y)es or (n)o?  ')
+        if question == 'y':
+            self.players_out += 1
+            player.out = True
+            print(str(player) + ' escaped the table with ' + str(player.money) + " coins left!!!")
 
     def play(self, players, dealer):
         for player in players:
-            self.start_draw(player)
+            if player.out != True:
+                self.start_bet(player)
+                self.start_draw(player)
+                self.special_options(player)
+        if self.players_out == len(players):
+            print("THE HOUSE ALWAYS WINS")
+            self.game_over = True
+            return ''
         self.start_draw(dealer)
+        self.pot += self.entry_bet
         winner = []
 
         while (winner == []):
             self.round(players)
             winner = self.determine_winner(players)
+
+        for player in players:
+            player.hand = []
+            player.stay = False
+            player.bust = False
+        dealer.hand = []
+        dealer.bust = False
+        dealer.stay = False
+        self.done = 0
+        self.pot = 0
+        winner = []
+        
 
 
 
@@ -244,5 +299,7 @@ class Game:
 
 new_game = Game(SUITS, RANKS)
 new_game.deck.shuffle()
-new_game.play(new_game.players, new_game.dealer)
+count = 0
+while new_game.game_over == False:
+    new_game.play(new_game.players, new_game.dealer)
 
